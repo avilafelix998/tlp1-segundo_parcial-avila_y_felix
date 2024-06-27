f0ed382cfb78c3766d65d3fb7f6f55498319a705
felix <avilafelix998@gmail.com>
2024-06-27 15:12:16 -0300

    mostré la base de datos en el servidor y mostré por id

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 //importar express
 const express = require("express");
 const app = express();
-const database = require("./database.js")
+const database = require("./database")
 // middlewares
 app.use(express.json());
 app.use(express.text());
-
+//mostrar la base de datos en el servidor 
+app.get("/products",(req,res)=>{
+    res.json(database)
+    
+    });
+    // mostrar por id
+    app.get("/products/:id",(req,res)=>{
+    const id = req.params.id;
+    res.json(database[id]);
+    })
 // se escucha el servidor 
 app.listen(3000, ()=>{
     console.log("servidor funcionando en puerto ",3000);
--- src/database.js
+++ src/database.js
@@ -1,1 +1,9 @@
 const databasebd = ("express");
+[
+    {
+"id": 1,
+"name": "juan",
+"quantity": 2,
+"price": 2.2
+    }
+]
